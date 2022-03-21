72b7881f49673c9770a1b685585fa6880206dc85
0x41gawor <41gawor@gmail.com>
2022-03-21 19:55:27 +0100

    feat(app): add port as app.run param

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +7,5 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
