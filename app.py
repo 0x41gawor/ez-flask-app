c3628f4b1e6932aef113eb16b71b50031192f1ab
0x41gawor <41gawor@gmail.com>
2022-03-23 21:14:08 +0100

    feat(app): use mongodb in code

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
+from pymongo import MongoClient
 app = Flask(__name__)
+
+def get_db():
+    conn = MongoClient('mongodb://ejek:ejek@localhost:27017/')
+    db = conn['ez_flask_app_db']
+    return db
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    db = get_db()
+    _animals = db.ez_flask_app_db.find()
+    
+    animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
+    return jsonify({"animals": animals})
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
